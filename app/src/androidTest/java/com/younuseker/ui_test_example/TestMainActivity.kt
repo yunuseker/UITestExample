3ba08b019631e7b4c4b1d3ec49c20b1975a19c22
yunuseker <younuseker@gmail.com>
2018-11-15 00:27:19 +0300

    first test

--- app/src/androidTest/java/com/younuseker/ui_test_example/TestMainActivity.kt
+++ app/src/androidTest/java/com/younuseker/ui_test_example/TestMainActivity.kt
@@ -1,9 +1,16 @@
 package com.younuseker.ui_test_example
 
+import android.support.test.espresso.Espresso
+import android.support.test.espresso.ViewAction
+import android.support.test.espresso.action.ViewActions
+import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+
 
 /**
  * Created by yunuseker on 14.11.2018.
@@ -16,4 +23,16 @@ class TestMainActivity {
     @Rule
     @JvmField
     val activity = ActivityTestRule<MainActivity>(MainActivity::class.java)
+
+    @Test
+    fun editText(){
+        Espresso.onView(withId(R.id.et_email))
+                .perform(ViewActions.typeText("yunus"))
+
+        Espresso.onView(withId(R.id.et_password))
+                .perform(ViewActions.typeText("seker"))
+
+        Espresso.onView(withId(R.id.btn_login))
+                .perform(ViewActions.click())
+    }
 }
--- app/src/main/java/com/younuseker/ui_test_example/MainActivity.kt
+++ app/src/main/java/com/younuseker/ui_test_example/MainActivity.kt
@@ -2,11 +2,18 @@ package com.younuseker.ui_test_example
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.younuseker.ui_test_example.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.btn_login).setOnClickListener({
+            Toast.makeText(this,"test success",Toast.LENGTH_LONG).show()
+        })
     }
 }
